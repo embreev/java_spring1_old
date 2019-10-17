8401a9940d6674e5a17ac76e8d27bd90a24ff94b
e.m.breev@gmail.com <e.m.breev@gmail.com>
2019-10-17 16:39:20 +0300

    hw1_xml

--- spring/src/main/java/ru/breev/Client.java
+++ spring/src/main/java/ru/breev/Client.java
@@ -3,9 +3,9 @@ package ru.breev;
 public class Client
 {
     public static void main(String[] args) {
-        Camera camera = new Camera();
-        CameraRoll cameraRoll = new ColorCameraRoll();
-        camera.setCameraRoll(cameraRoll);
-        camera.doPhotograph();
+//        Camera camera = new Camera();
+//        CameraRoll cameraRoll = new ColorCameraRoll();
+//        camera.setCameraRoll(cameraRoll);
+//        camera.doPhotograph();
     }
 }
